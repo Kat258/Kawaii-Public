70f85090edd1e8a516c93f26fd471ece820932fc
MarginSphydro <190247237+MarginSphydro@users.noreply.github.com>
2025-08-04 11:25:17 +0800

    Size & Hight

--- src/main/java/dev/kizuna/asm/mixins/MixinPlayerListHud.java
+++ src/main/java/dev/kizuna/asm/mixins/MixinPlayerListHud.java
@@ -1,23 +1,61 @@
 package dev.kizuna.asm.mixins;
 
+import com.llamalad7.mixinextras.sugar.Local;
+import com.llamalad7.mixinextras.sugar.ref.LocalIntRef;
 import dev.kizuna.mod.modules.impl.render.BetterTab;
 import net.minecraft.client.gui.hud.PlayerListHud;
 import net.minecraft.client.network.PlayerListEntry;
 import net.minecraft.text.Text;
 import org.spongepowered.asm.mixin.Mixin;
+import org.spongepowered.asm.mixin.Shadow;
 import org.spongepowered.asm.mixin.injection.At;
+import org.spongepowered.asm.mixin.injection.Constant;
 import org.spongepowered.asm.mixin.injection.Inject;
+import org.spongepowered.asm.mixin.injection.ModifyConstant;
 import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
 import org.spongepowered.asm.mixin.injection.callback.CallbackInfoReturnable;
 
+import java.util.List;
+
 @Mixin(PlayerListHud.class)
 public abstract class MixinPlayerListHud {
+
+    @Shadow
+    protected abstract List<PlayerListEntry> collectPlayerEntries();
+
+    @Inject(method = "collectPlayerEntries", at = @At("RETURN"), cancellable = true)
+    private void modifyPlayerCount(CallbackInfoReturnable<List<PlayerListEntry>> info) {
+        BetterTab module = BetterTab.INSTANCE;
+        if (module.isOn()) {
+            List<PlayerListEntry> entries = info.getReturnValue();
+            double maxSize = module.tabSize.getValue();
+            if (entries.size() > maxSize) {
+                info.setReturnValue(entries.subList(0, (int) maxSize));
+            }
+        }
+    }
 
     @Inject(method = "getPlayerName", at = @At("HEAD"), cancellable = true)
     public void getPlayerName(PlayerListEntry playerListEntry, CallbackInfoReturnable<Text> info) {
         if (BetterTab.INSTANCE.isOn()) {
             info.setReturnValue(BetterTab.INSTANCE.getPlayerName(playerListEntry));
         }
+    }
+
+    @Inject(method = "render", at = @At(value = "INVOKE", target = "Ljava/lang/Math;min(II)I", shift = At.Shift.BEFORE))
+    private void modifyHeight(CallbackInfo ci, @Local(ordinal = 5) LocalIntRef o, @Local(ordinal = 6) LocalIntRef p) {
+        BetterTab module = BetterTab.INSTANCE;
+        if (!module.isOn()) return;
+
+        int newO;
+        int newP = 1;
+        int totalPlayers = newO = this.collectPlayerEntries().size();
+        while (newO > module.tabHeight.getValue()) {
+            newO = (totalPlayers + ++newP - 1) / newP;
+        }
+
+        o.set(newO);
+        p.set(newP);
     }
 
     @Inject(method = "renderLatencyIcon", at = @At("HEAD"), cancellable = true)
