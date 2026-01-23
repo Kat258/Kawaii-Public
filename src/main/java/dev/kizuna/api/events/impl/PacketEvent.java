75a39708a01f12f3c0fce85b8e89268d9b29d00d
MarginSphydro <vs.mark@vip.qq.com>
2026-01-23 21:24:30 +0800

    fix

--- src/main/java/dev/kizuna/api/events/impl/PacketEvent.java
+++ src/main/java/dev/kizuna/api/events/impl/PacketEvent.java
@@ -4,26 +4,29 @@ import dev.kizuna.api.events.Event;
 import net.minecraft.network.packet.Packet;
 
 public class PacketEvent extends Event {
-    private final Packet<?> packet;
 
-    public PacketEvent(Packet<?> packet, Event.Stage stage) {
+    private final Packet<?> packet;
+    public PacketEvent(Packet<?> packet, Stage stage) {
         super(stage);
         this.packet = packet;
     }
-
-    public Packet<?> getPacket() {
-        return this.packet;
+    public <T extends Packet<?>> T getPacket() {
+        return (T) packet;
+    }
+    public static class Send extends PacketEvent {
+        public Send(Packet<?> packet) {
+            super(packet, Stage.Pre);
+        }
+    }
+    public static class SendPost extends PacketEvent {
+        public SendPost(Packet<?> packet) {
+            super(packet, Stage.Post);
+        }
     }
 
     public static class Receive extends PacketEvent {
         public Receive(Packet<?> packet) {
-            super(packet, Event.Stage.Pre);
-        }
-    }
-
-    public static class Send extends PacketEvent {
-        public Send(Packet<?> packet) {
-            super(packet, Event.Stage.Pre);
+            super(packet, Stage.Pre);
         }
     }
 
